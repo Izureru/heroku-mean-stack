1ded7fb6dc4596c49476fa61ae2054fbe72e399d
Izu Reru <izureru@jream-machine.local>
2020-07-21 21:22:55 +0100

    implemented remaining endpoints

--- server.js
+++ server.js
@@ -76,10 +76,35 @@ app.post("/api/weddings", function(req, res) {
  */
 
 app.get("/api/weddings/:id", function(req, res) {
+  db.collection(WEDDINGS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get wedding");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
 });
 
 app.put("/api/weddings/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(WEDDINGS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update wedding");
+    } else {
+      updateDoc._id = req.params.id;
+      res.status(200).json(updateDoc);
+    }
+  });
 });
 
 app.delete("/api/weddings/:id", function(req, res) {
+  db.collection(WEDDINGS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete wedding");
+    } else {
+      res.status(200).json(req.params.id);
+    }
+  });
 });
